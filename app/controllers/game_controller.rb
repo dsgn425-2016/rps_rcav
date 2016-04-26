b6031d5c60311e6671edfee717610f552982c0ca
ad1441 <adivakaran2017@kellogg.northwestern.edu>
2016-04-25 23:58:25 -0500

    final-for-review

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -4,12 +4,18 @@ class GameController < ApplicationController
     if comp_move_num == 0
       @computer_move = "Rock"
       @outcome = "You tied!"
+      @comp_image_url = "http://localhost:3000/assets/images/rock.png"
+      @label_color = "label-warning"
     elsif comp_move_num ==1
       @computer_move = "Paper"
       @outcome = "You lost!"
+      @comp_image_url = "http://localhost:3000/assets/images/paper.png"
+      @label_color = "label-danger"
     else
       @computer_move = "Scissors"
       @outcome = "You won!"
+      @comp_image_url = "http://localhost:3000/assets/images/scissors.png"
+      @label_color = "label-success"
     end
     render 'fist'
   end
@@ -19,12 +25,18 @@ class GameController < ApplicationController
     if comp_move_num == 0
       @computer_move = "Rock"
       @outcome = "You won!"
+      @comp_image_url = "http://localhost:3000/assets/images/rock.png"
+      @label_color = "label-success"
     elsif comp_move_num ==1
       @computer_move = "Paper"
       @outcome = "You tied!"
+      @comp_image_url = "http://localhost:3000/assets/images/paper.png"
+      @label_color = "label-warning"
     else
       @computer_move = "Scissors"
       @outcome = "You lost!"
+      @comp_image_url = "http://localhost:3000/assets/images/scissors.png"
+      @label_color = "label-danger"
     end
     render 'flat'
   end
@@ -34,12 +46,18 @@ class GameController < ApplicationController
     if comp_move_num == 0
       @computer_move = "Rock"
       @outcome = "You lost!"
+      @comp_image_url = "http://localhost:3000/assets/images/rock.png"
+      @label_color = "label-danger"
     elsif comp_move_num ==1
       @computer_move = "Paper"
       @outcome = "You won!"
+      @comp_image_url = "http://localhost:3000/assets/images/paper.png"
+      @label_color = "label-success"
     else
       @computer_move = "Scissors"
       @outcome = "You tied!"
+      @comp_image_url = "http://localhost:3000/assets/images/scissors.png"
+      @label_color = "label-warning"
     end
     render 'cut'
   end
