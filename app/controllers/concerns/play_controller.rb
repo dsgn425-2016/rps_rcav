e7101ecc5e674de2304b72d2a56200aadbe8defd
Tyler Hardy <thardy2017@kellogg.northwestern.edu>
2016-04-26 15:53:10 -0500

    making progress

--- app/controllers/concerns/play_controller.rb
+++ app/controllers/concerns/play_controller.rb
@@ -1,6 +1,34 @@
 class PlayController < ApplicationController
 
   def rock
+
+    render("rock.html.erb")
+  end
+
+  def paper
+
+    render("rock.html.erb")
+  end
+
+  def scissors
+    
     render("rock.html.erb")
   end
 end
+
+
+# if user_move == computer_move
+#   puts "You tied!"
+# elsif user_move == "paper" && computer_move == "rock"
+#   puts "You win!"
+# elsif user_move == "paper" && computer_move == "scissors"
+#   puts "You lose!"
+# elsif user_move == "scissors" && computer_move == "rock"
+#   puts "You lose!"
+# elsif user_move == "scissors" && computer_move == "paper"
+#   puts "You win!"
+# elsif user_move == "rock" && computer_move == "paper"
+#   puts "You lose!"
+# elsif user_move == "rock" && computer_move == "scissors"
+#   puts "You win!"
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 get("/rock", {:controller => "play", :action => "rock"})
-
+get("/paper", {:controller => "play", :action => "paper"})
+get("/scissors", {:controller => "play", :action => "scissors"})
 
 end
