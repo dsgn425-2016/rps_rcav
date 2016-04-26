06285a1eb5040c7c07f21f501e42bdf82143ea5e
RamyaKellogg <rsethurathinam2017@kellogg.northwestern.edu>
2016-04-25 22:43:57 -0500

    Part 1

---
+++ app/controllers/rps_controller.rb
@@ -0,0 +1,15 @@
+class RpsController < ApplicationController
+
+  def rock
+    render("rock.html.erb")
+  end
+
+  def paper
+    render("paper.html.erb")
+  end
+
+  def scissors
+    render("scissors.html.erb")
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  get("/rock",{ :controller => "rps", :action => "rock"})
+  get("/paper",{ :controller => "rps", :action => "paper"})
+  get("/scissors",{ :controller => "rps", :action => "scissors"})
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
