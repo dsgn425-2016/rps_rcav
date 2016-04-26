3a2484f291e7bd7f450dde82529c965294b97594
StanleyTack <stanley.tack@users.noreply.github.com>
2016-04-25 22:30:34 -0500

    created html structure

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,14 @@
+class GameController < ApplicationController
+
+  def play_rock
+    render("play_rock.html.erb")
+  end
+
+  # def play_paper
+  #   render 'play_paper'
+  # end
+  #
+  # def play_scissors
+  #   render 'play_scissors'
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  get("/rock", {:controller => "game", :action => "play_rock"})
+  # get("/paper", :controller => "game", :action => "play_paper")
+  # get("/scissors", :controller => "game", :action => "play_scissors")
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
