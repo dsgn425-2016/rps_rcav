d65d2370c310cb6d0ddbcb8b848141908c462c24
RamyaKellogg <rsethurathinam2017@kellogg.northwestern.edu>
2016-04-27 22:50:20 -0500

    Ready for Review

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get("/rock",{ :controller => "rps", :action => "rock"})
   get("/paper",{ :controller => "rps", :action => "paper"})
   get("/scissors",{ :controller => "rps", :action => "scissors"})
+  get("/",{ :controller => "rps", :action => "rock"})
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
