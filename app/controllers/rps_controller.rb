7660a1f20398ac2b4a3a1e909ffc42fde506fa72
lucialiu2017 <lliu2017@kellogg.northwestern.edu>
2016-05-02 14:08:46 -0500

    final_v3

--- app/controllers/rps_controller.rb
+++ app/controllers/rps_controller.rb
@@ -1,92 +1,56 @@
 class RpsController < ApplicationController
-  def game_output
-    @your_image = "assets/images/#{@your_move}.png"
-    @computer_image = "assets/images/#{@computer_move}.png"
-    render ("output.html.erb")
+
+  def tied
+    @result_words= "You tied!"
+    @result_label = "label-warning"
+  end
+
+  def lose
+    @result_words = "You lose!"
+    @result_label="label-danger"
+  end
+
+  def won
+    @result_words = "You won!"
+    @result_label="label-success"
   end
 
   def play_rock
-    @your_move = "Rock"
-    @computer_move = ["Rock", "Paper", "Scissors"].sample
+    @your_move = "rock"
+    @computer_move = ["rock", "paper", "scissors"].sample
     if @computer_move==@your_move
-      @result_words= "You tied!"
-      @result_label = "label-warning"
-    elsif @computer_move=="Paper"
-      @result_words = "You lose!"
-      @result_label="label-danger"
+      tied
+    elsif @computer_move=="paper"
+      lose
     else
-      @result_words = "You won!"
-      @result_label="label-success"
+      won
     end
-    game_output
+    render ("output.html.erb")
   end
 
   def play_paper
-    @your_move = "Paper"
-    @computer_move = ["Rock", "Paper", "Scissors"].sample
+    @your_move = "paper"
+    @computer_move = ["rock", "paper", "scissors"].sample
     if @computer_move==@your_move
-      @result_words= "You tied!"
-      @result_label = "label-warning"
-    elsif @computer_move=="Scissors"
-      @result_words = "You lose!"
-      @result_label="label-danger"
+      tied
+    elsif @computer_move=="scissors"
+      lose
     else
-      @result_words = "You won!"
-      @result_label="label-success"
+      won
     end
-    game_output
+    render ("output.html.erb")
   end
 
   def play_scissors
-    @your_move = "Scissors"
-    @computer_move = ["Rock", "Paper", "Scissors"].sample
+    @your_move = "scissors"
+    @computer_move = ["rock", "paper", "scissors"].sample
     if @computer_move==@your_move
-      @result_words= "You tied!"
-      @result_label = "label-warning"
-    elsif @computer_move=="Rock"
-      @result_words = "You lose!"
-      @result_label="label-danger"
+      tied
+    elsif @computer_move=="rock"
+      lose
     else
-      @result_words = "You won!"
-      @result_label="label-success"
+      won
     end
-    game_output
+    render ("output.html.erb")
   end
 end
-
-#   def play_rock
-#     @your_move = "rock"
-#     @computer_move = ["rock", "paper", "scissors"].sample
-#     if @computer_move==@your_move
-#       render("tied.html.erb")
-#     elsif @computer_move=="paper"
-#       render("lose.html.erb")
-#     else
-#       render("won.html.erb")
-#     end
-#   end
-#
-#   def play_paper
-#     @your_move = "paper"
-#     @computer_move = ["rock", "paper", "scissors"].sample
-#     if @computer_move==@your_move
-#       render("tied.html.erb")
-#     elsif @computer_move=="scissors"
-#       render("lose.html.erb")
-#     else
-#       render("won.html.erb")
-#     end
-#   end
-#
-#   def play_scissors
-#     @your_move = "scissors"
-#     @computer_move = ["rock", "paper", "scissors"].sample
-#     if @computer_move==@your_move
-#       render("tied.html.erb")
-#     elsif @computer_move=="rock"
-#       render("lose.html.erb")
-#     else
-#       render("won.html.erb")
-#     end
-#   end
-# end
