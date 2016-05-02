c84c72a5b04091db74746b0b38473cd018abd1da
Ahalya Vijay <avijay2017@users.noreply.github.com>
2016-05-02 10:04:16 -0500

    Few more changes

--- app/controllers/rps_paper_controller.rb
+++ app/controllers/rps_paper_controller.rb
@@ -5,7 +5,15 @@ class RpsPaperController < ApplicationController
   end
 
   def paper
-
+    @computer_move = ["rock","paper","scissors"].sample
+    if @computer_move == "paper"
+      @result = "You're tied!"
+    elsif @computer_move == "rock"
+      @result = "You win!"
+    elsif @computer_move == "scissors"
+      @result = "You lose!"
+    end
+    render("paper.html.erb")
   end
 
 end
--- app/controllers/rps_scissors_controller.rb
+++ app/controllers/rps_scissors_controller.rb
@@ -1,11 +1,18 @@
 class RpsScissorsController < ApplicationController
 
   def scissors_form
-    render("scissors.html.erb")
+    render("scissors_form.html.erb")
   end
 
   def scissors
-
-  end
+    @computer_move = ["rock","paper","scissors"].sample
+    if @computer_move == "rock"
+      @result = "You lose!"
+    elsif @computer_move == "paper"
+      @result = "You win!"
+    elsif @computer_move == "scissors"
+      @result = "You're tied!"
+    end
+    render("scissors.html.erb")  end
 
 end
