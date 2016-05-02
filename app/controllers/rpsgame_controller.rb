3f2fe1e846281457cf6787d8ff5ebc268b560f74
Ahalya Vijay <avijay2017@users.noreply.github.com>
2016-05-02 12:08:07 -0500

    HTML code finalized

--- app/controllers/rpsgame_controller.rb
+++ app/controllers/rpsgame_controller.rb
@@ -3,18 +3,17 @@ class RpsgameController < ApplicationController
   def rock
     choices = ["rock","scissors","paper"]
     @computer_move = choices.sample
-    @user_image = "/public/assets/images/rock.png"
     if @computer_move == "rock"
       @result = "You're tied!"
-      @comp_image = "/public/assets/images/rock.png"
+      @computer_image = "/assets/images/rock.png"
       @label = "label-warning"
     elsif @computer_move == "paper"
       @result = "You lose!"
-      @comp_image = "/public/assets/images/paper.png"
+      @computer_image = "/assets/images/paper.png"
       @label = "label-danger"
     elsif @computer_move == "scissors"
       @result = "You win!"
-      @comp_image = "/public/assets/images/scissors.png"
+      @computer_image = "/assets/images/scissors.png"
       @label = "label-success"
     end
     render("rock.html.erb")
@@ -23,18 +22,17 @@ class RpsgameController < ApplicationController
   def paper
     choices = ["rock","scissors","paper"]
     @computer_move = choices.sample
-    @user_image = "/public/assets/images/paper.png"
     if @computer_move == "rock"
       @result = "You win!"
-      @comp_image = "/public/assets/images/rock.png"
+      @computer_image = "/assets/images/rock.png"
       @label = "label-success"
     elsif @computer_move == "paper"
       @result = "You're tied!"
-      @comp_image = "/public/assets/images/paper.png"
+      @computer_image = "/assets/images/paper.png"
       @label = "label-warning"
     elsif @computer_move == "scissors"
       @result = "You lose!"
-      @comp_image = "/public/assets/images/scissors.png"
+      @computer_image = "/assets/images/scissors.png"
       @label = "label-danger"
     end
     render("paper.html.erb")
@@ -43,18 +41,17 @@ class RpsgameController < ApplicationController
   def scissors
     choices = ["rock","scissors","paper"]
     @computer_move = choices.sample
-    @user_image = "/public/assets/images/scissors.png"
     if @computer_move == "rock"
       @result = "You lose!"
-      @comp_image = "/public/assets/images/rock.png"
+      @computer_image = "/assets/images/rock.png"
       @label = "label-danger"
     elsif @computer_move == "paper"
       @result = "You win!"
-      @comp_image = "/public/assets/images/paper.png"
+      @computer_image = "/assets/images/paper.png"
       @label = "label-success"
     elsif @computer_move == "scissors"
       @result = "You're tied!"
-      @comp_image = "/public/assets/images/scissors.png"
+      @computer_image = "/assets/images/scissors.png"
       @label = "label-warning"
     end
     render("scissors.html.erb")
