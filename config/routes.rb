cc335a5516ee5c8f1f88762a519d0bcc4d82c03e
kyang2017 <kyang2017@kellogg.northwestern.edu>
2016-05-02 01:56:53 -0500

    For Review

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  get("/", { :controller => "moves", :action => "rock"})
+  get("/rock", { :controller => "moves", :action => "rock"})
+  get("/scissors", { :controller => "moves", :action => "scissors"})
+  get("/paper", { :controller => "moves", :action => "paper"})
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
