2547235a7be5d13a51edde195d97934ca54e0fda
Ahalya Vijay <avijay2017@users.noreply.github.com>
2016-05-02 08:47:28 -0500

    Set up initial configuration to display Hi

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  get("/", :controller => 'rpsgame', :action => 'rock')
+  get("/rock", :controller => 'rpsgame', :action => 'rock')
+  get("/paper", :controller => 'rpsgame', :action => 'paper')
+  get("/scissors", :controller => 'rpsgame', :action => 'scissors')
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
