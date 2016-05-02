1afa69f2064282bd0cebe388eb99a0a3478216e6
Anita Prasad <aprasad2017@kellogg.northwestern.edu>
2016-05-01 19:32:14 -0500

    Updated with functionality
    Basic logic for three pages coded in (no formatting or linkage between
    pages)

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -1,17 +1,43 @@
 class PlayController < ApplicationController
-def play_rock
-  @test_rock = "hi"
-render("play_rock.html.erb")
-end
+  def play_rock
+    @user_move = "rock"
+    @computer_move = ["rock", "paper", "scissors"].sample
+    if @user_move == @computer_move
+      @result = "tied"
+    elsif @computer_move == "scissors"
+      @result = "won"
+    else
+      @result = "lose"
+    end
 
-def play_paper
-  @test_paper = "HI"
-render("play_paper.html.erb")
-end
+    render("play_rock.html.erb")
+  end
 
-def play_scissors
-  @test_scissors = "Hi!"
-render("play_scissors.html.erb")
-end
+  def play_paper
+    @user_move = "paper"
+    @computer_move = ["rock", "paper", "scissors"].sample
+    if @user_move == @computer_move
+      @result = "tied"
+    elsif @computer_move == "rock"
+      @result = "won"
+    else
+      @result = "lose"
+    end
+    render("play_paper.html.erb")
+  end
+
+  def play_scissors
+    @user_move = "scissors"
+    @computer_move = ["rock", "paper", "scissors"].sample
+    if @user_move == @computer_move
+      @result = "tied"
+    elsif @computer_move == "paper"
+      @result = "won"
+    else
+      @result = "lose"  
+    end
+
+    render("play_scissors.html.erb")
+  end
 
 end
