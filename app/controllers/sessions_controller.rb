00ff7538d8ea385e136afe18878b21913e96fa1d
J <B>
2016-05-08 15:50:29 -0500

    done!
    yay!

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,39 +1,46 @@
 class SessionsController < ApplicationController
   #you always need "controller" after the name here
   def rock
-    render ("rocks.html.erb")
-    @computer_move = ["rock", "paper", "scissors"].sample
-    @user_move = ["rock"]
+    @computer_move = ["Rock", "Paper", "Scissors"].sample
+    # @computer_image = "assets/images/#{@computer_move}.png"
+    @user_move = "Rock"
+    @rps_image= "assets/images/rock.png"
     if @user_move == @computer_move
       @result = "have tied the computer in a game of chance"
-    elsif @user_move == "rock" && @computer_move == "paper"
-      @result = "have been destroyed"
-    elsif @user_move == "rock" && @computer_move == "scissors"
-      @result = "have dominated the competition"
+    elsif @user_move == "Rock" && @computer_move == "Paper"
+      @result = "You have been destroyed"
+    elsif @user_move == "Rock" && @computer_move == "Scissors"
+      @result = "You have dominated the competition"
     end
+    render ("rocks.html.erb")
   end
 
   def paper
-    render ("paper.html.erb")
-      @user_move = ["paper"]
+    @computer_move = ["Rock", "Paper", "Scissors"].sample
+    @user_move = "Paper"
+    @rps_image= "assets/images/paper.png"
     if @user_move == @computer_move
-      @result = "have tied the computer in a game of chance"
-    elsif @user_move == "paper" && @computer_move == "rock"
-      @result = "have dominated the competition"
-    elsif @user_move == "paper" && @computer_move == "scissors"
+      @result = "You have tied the computer in a game of chance"
+    elsif @user_move == "Paper" && @computer_move == "Rock"
+      @result = "You have dominated the competition"
+    elsif @user_move == "Paper" && @computer_move == "Scissors"
+      @result = "You are bad at this game, and should feel bad"
     end
+    render ("paper.html.erb")
   end
 
   def scissors
-    render ("scissors.html.erb")
-      @user_move = ["scissors"]
+    @computer_move = ["Rock", "Paper", "Scissors"].sample
+    @user_move = "Scissors"
+    @rps_image= "assets/images/scissors.png"
     if @user_move == @computer_move
       @result = "have tied the computer in a game of chance"
-    elsif @user_move == "scissors" && @computer_move == "paper"
-      @result = "have dominated the competition"
-    elsif @user_move == "scissors" && @computer_move == "rock"
-      @result = "have been destroyed"
+    elsif @user_move == "Scissors" && @computer_move == "Paper"
+      @result = "You have dominated the competition"
+    elsif @user_move == "Scissors" && @computer_move == "Rock"
+      @result = "You have been destroyed"
     end
+    render ("scissors.html.erb")
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   #things start with the route that perform an action to call a view template
 
+  get("/", { :controller => "sessions", :action => "rock" })
+
   get("/rock", { :controller => "sessions", :action => "rock" })
 
   get("/paper", { :controller => "sessions", :action => "paper" })
@@ -10,7 +12,7 @@ Rails.application.routes.draw do
 
 
   #when you get to the dynamic routes, you use get("/:something")
-#params{:move]}
+  #params{:move]}
 
 end
 
@@ -18,57 +20,57 @@ end
 
 
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+# The priority is based upon order of creation: first created -> highest priority.
+# See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+# You can have the root of your site routed with "root"
+# root 'welcome#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+# Example of regular route:
+#   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+# Example of named route that can be invoked with purchase_url(id: product.id)
+#   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+# Example resource route (maps HTTP verbs to controller actions automatically):
+#   resources :products
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+# Example resource route with options:
+#   resources :products do
+#     member do
+#       get 'short'
+#       post 'toggle'
+#     end
+#
+#     collection do
+#       get 'sold'
+#     end
+#   end
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+# Example resource route with sub-resources:
+#   resources :products do
+#     resources :comments, :sales
+#     resource :seller
+#   end
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+# Example resource route with more complex sub-resources:
+#   resources :products do
+#     resources :comments
+#     resources :sales do
+#       get 'recent', on: :collection
+#     end
+#   end
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+# Example resource route with concerns:
+#   concern :toggleable do
+#     post 'toggle'
+#   end
+#   resources :posts, concerns: :toggleable
+#   resources :photos, concerns: :toggleable
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+# Example resource route within a namespace:
+#   namespace :admin do
+#     # Directs /admin/products/* to Admin::ProductsController
+#     # (app/controllers/admin/products_controller.rb)
+#     resources :products
+#   end
